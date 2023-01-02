d4143e92e4a3793a05127d807efde40b5eeb9dd6
brannanz <76947244+brannanz@users.noreply.github.com>
2023-01-02 20:42:34 +0000

    Update ToolbarListener.cpp

--- src/source/Listener/ToolbarListener.cpp
+++ src/source/Listener/ToolbarListener.cpp
@@ -10,7 +10,7 @@ void ToolbarListener::onButton1MouseClick(BaseButtonInstance* btn)
 	for(size_t i = 0; i < btns.size(); i++)
 		btns[i]->selected = false;
 		button->selected = true;
-		if(button->title == "Model") g_usableApp->navigateToolbox("http://androdome.com/res/ClientToolbox.php");
+		if(button->title == "Model") g_usableApp->navigateToolbox("http://localhost/Client/Toolbox.php");
 		else if(button->title == "Surface") g_usableApp->navigateToolbox(GetFileInPath("/content/page/surface.html"));
 		else if(button->title == "Color") g_usableApp->navigateToolbox(GetFileInPath("/content/page/color.html"));
 		else if(button->title == "Controller") g_usableApp->navigateToolbox(GetFileInPath("/content/page/controller.html"));
